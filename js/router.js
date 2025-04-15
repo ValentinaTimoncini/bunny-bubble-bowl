4576d22e2474f37d588819d278596b61136799b7
ValentinaTimoncini <sabrinausvelli@gmail.com>
2025-04-15 18:34:10 +0200

    Updates

--- js/router.js
+++ js/router.js
@@ -1,3 +1,4 @@
+const x_repo = '/bunny-bubble-bowl'
 const x_sitemap = [
     {
         x_hash: '',
@@ -41,13 +42,13 @@ async function x_build(x_currhash){
             x_site = site
         }
     })
-    let x_content_response = await fetch('/views/'+x_site.x_view)
+    let x_content_response = await fetch(x_repo+'/views/'+x_site.x_view)
     let x_content_html = await x_content_response.text()
     document.getElementById("app").innerHTML = ''
     document.getElementById("app").insertAdjacentHTML('afterbegin', x_content_html)
     //FOOTER
     if(document.getElementById("foot").innerHTML.length === 0) {
-        let x_footer_response = await fetch('/components/footer.html')
+        let x_footer_response = await fetch(x_repo+'/components/footer.html')
         let x_footer_html = await x_footer_response.text()
         document.getElementById("foot").insertAdjacentHTML('afterbegin', x_footer_html)
     }
@@ -56,19 +57,19 @@ async function x_build(x_currhash){
     x_existingStyles.forEach(x_style => x_style.remove());
     let x_link = document.createElement('link')
     x_link.rel = "stylesheet";
-    x_link.href = "/css/"+x_site.x_css;
+    x_link.href = x_repo+"/css/"+x_site.x_css;
     document.head.append(x_link);
     //SCRIPT
     const x_existingScripts = document.querySelectorAll('script:not([data-default="1"])');
     x_existingScripts.forEach(x_script => x_script.remove());
     let x_script = document.createElement('script')
-    x_script.src = "/src/js/"+x_site.x_script;
+    x_script.src = x_repo+"/js/"+x_site.x_script;
     document.body.append(x_script);
 
     x_stop_laoding()
 }
 async function x_start_laoding(){
-    let x_loader_response = await fetch('/components/loader.html')
+    let x_loader_response = await fetch(x_repo+'/components/loader.html')
     let x_loader_html = await x_loader_response.text()
     document.body.insertAdjacentHTML('afterbegin', x_loader_html)
 }
