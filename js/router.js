1af6eb2c4015d8445585539e46d6c371f4c50066
ValentinaTimoncini <sabrinausvelli@gmail.com>
2025-04-17 00:08:42 +0200

    Updates

--- js/router.js
+++ js/router.js
@@ -66,7 +66,7 @@ async function x_build(x_currhash){
     x_script.src = x_repo+"/js/"+x_site.x_script;
     document.body.append(x_script);
 
-    x_stop_laoding()
+    //x_stop_laoding()
 }
 async function x_start_laoding(){
     let x_loader_response = await fetch(x_repo+'/components/loader.html')
